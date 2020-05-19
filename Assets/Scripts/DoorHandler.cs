b1ec4708e320ca908fc22bb592c7ce65d4631cc1
minabilgin <begum.bilgin@isik.edu.tr>
2020-05-19 17:45:50 +0300

    DoorHandler, Collider, Pivots added.
    * OpenPivot added for the animation of doors' opening and the pivot's been taken from the middle to the appropriate positions.
    * DoorHandler Script added for doors' opening rotations, opening speeds, if they're open or not etc.
    * Colliders added for each door to be able to have the connection with doors.

---
+++ Assets/Scripts/DoorHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class DoorHandler : MonoBehaviour
+{
+    [Header("CONST")]
+    public string levelName = "";
+    public float speed = 5f;
+
+    [Header("OBJECTS")]
+    public Transform animPivotTrans;
+    public Transform animPivotTransForGate5;
+
+
+    private bool isOpen;
+
+    private void OnMouseOver()
+    {
+        isOpen = true;
+    }
+    private void OnMouseExit()
+    {
+        isOpen = false;
+    }
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+    private void Update()
+    {
+        if (isOpen)
+        {
+            animPivotTrans.localRotation = Quaternion.Lerp(animPivotTrans.localRotation, Quaternion.Euler(new Vector3(0, 25f, 0)), speed * Time.deltaTime);
+            if (animPivotTransForGate5 != null)
+            {
+                animPivotTransForGate5.localRotation = Quaternion.Lerp(animPivotTransForGate5.localRotation, Quaternion.Euler(new Vector3(0, -25f, 0)), speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            animPivotTrans.localRotation = Quaternion.Lerp(animPivotTrans.localRotation, Quaternion.Euler(Vector3.zero), speed * Time.deltaTime);
+            if (animPivotTransForGate5 != null)
+            {
+                animPivotTransForGate5.localRotation = Quaternion.Lerp(animPivotTransForGate5.localRotation, Quaternion.Euler(Vector3.zero), speed * Time.deltaTime);
+            }
+        }
+    }
+
+
+}
