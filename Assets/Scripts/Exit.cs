1254d0ef04139647d4acd9bc6ad65d29cc3db336
minabilgin <begum.bilgin@isik.edu.tr>
2020-05-20 14:23:52 +0300

    Back and Exit
    Back and Exit buttons are added.

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    /*public void QuitGame() {
+         //Quit event wont be visible in Unity Editor. 
+                           //This is for me to understand that it quits.
+        Application.Quit();
+        Debug.Log("Quit");
+    }*/
+
+    /*void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+            Debug.Log("Quit.");
+        }
+    }*/
+}
