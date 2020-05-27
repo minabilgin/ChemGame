d0c8802455d48a65764f3cd0536be61a5bc9a057
minabilgin <begum.bilgin@isik.edu.tr>
2020-05-27 17:39:17 +0300

    Enterence Scene
    * Unused codes are deleted.
    * Speech bubble's been added.
    * Input field's been added for user to type a name on speech bubble.
    * Button to enter the home scene's been added.
    * Script for those actions' been added.

---
+++ Assets/Scripts/NameInputs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NameInputs : MonoBehaviour {
+    public InputField inputField; //place we will type the name
+    private string textDisplay; // where we display the output 
+    private string levelName = "Home"; //to open the home level
+
+    public void ButtonClick() //The function which we call with OnClick event.
+    {
+        textDisplay = inputField.text.ToString(); //Where we store the data of InputField object.
+        if (inputField.text != "")
+        {    
+            SceneManager.LoadScene(levelName);
+            Debug.Log("Merhaba, " + textDisplay); //Output log of the name user entered if the user typed a name
+        }
+        else
+        {
+            Debug.Log("Öğrenci adı girilmedi."); //If no name's been typed.
+        }
+    }
+}
