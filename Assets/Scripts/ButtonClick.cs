c83ab059a309a3b4036f3297f2af468ad500ca36
minabilgin <begum.bilgin@isik.edu.tr>
2020-06-16 23:58:07 +0300

    Monitors and level2
    Level2's buttons and it's scripts are added.
    Level 3 and 4's sprite renderers are added. They now can print images on monitor.
    New images for NaCl, H2O and CO2 are added.

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonClick : MonoBehaviour
+{
+    public static ButtonClick instance;
+    public bool ButtonOn = false;
+    public Button MyButton;
+    public Button OtherButton;
+    public bool isTrue = false;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    public void BeenClicked()
+    {
+        ButtonOn = !ButtonOn;
+
+        OtherButton.interactable = ButtonOn;
+        if (!ButtonOn)
+        {
+            MyButton.image.color = Color.cyan;
+            if (MyButton.tag == "T")
+            {
+                isTrue = true;
+                
+            }
+            else
+            {
+                isTrue = false;
+
+            }
+        }
+        else
+        {
+            MyButton.image.color = Color.white;
+            isTrue = false;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Level2TF.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Level2TF : MonoBehaviour
+{
+    public static Level2TF instance;
+
+    public GameObject truePanel;
+    public GameObject falsePanel;
+    public ButtonClick button1, button2, button3, button4, button5, button6;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+
+    }
+    public void CheckAnswers()
+    {
+        int correctCounter = 0;
+
+        if (button1.isTrue && button1.gameObject.tag == "T" && !button1.ButtonOn)
+        {
+            correctCounter++;
+        }
+        if (button2.isTrue && button2.gameObject.tag == "T" && !button2.ButtonOn)
+        {
+            correctCounter++;
+        }
+        if (button3.isTrue && button3.gameObject.tag == "T" && !button3.ButtonOn)
+        {
+            correctCounter++;
+        }
+        if (button4.isTrue && button4.gameObject.tag == "T" && !button4.ButtonOn)
+        {
+            correctCounter++;
+        }
+        if (button5.isTrue && button5.gameObject.tag == "T" && !button5.ButtonOn)
+        {
+            correctCounter++;
+        }
+        if (button6.isTrue && button6.gameObject.tag == "T" && !button6.ButtonOn)
+        {
+            correctCounter++;
+        }
+
+        if (correctCounter == 3)
+        {
+            truePanel.SetActive(true);
+        }
+        else
+        {
+            falsePanel.SetActive(true);
+        }
+
+        correctCounter = 0;
+    }
+
+ 
+}
